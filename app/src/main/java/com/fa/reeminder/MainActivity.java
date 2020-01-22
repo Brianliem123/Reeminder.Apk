cd5902585a148329038a7d15409550dee0c19dc4
brian <febrianliem123@gmail.com>
2020-01-22 15:26:58 +0700

    Remove Feature

--- app/src/main/java/com/fa/reeminder/MainActivity.java
+++ app/src/main/java/com/fa/reeminder/MainActivity.java
@@ -4,39 +4,91 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayAdapter<String> itemsAdapter;
+    private ArrayList<String> items;
+    private ListView lvItem;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final FloatingActionButton floatingactionbuttom = findViewById(R.id.fab);
-        floatingactionbuttom.setOnClickListener(new View.OnClickListener() {
+        initial();
+
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
-                alert.setTitle("Add New")
-                        .setMessage("Ingin menambahkan Catatan ? ")
-                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                addtask();
+            }
+        });
+
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItem.setAdapter(itemsAdapter);
+        lvItem.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final int which_item = position;
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setTitle("Apakah Anda Yakin ?")
+                        .setMessage("Apakah Anda Ingin Menghapusmya ? ")
+                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                finish();
+                                items.remove(which_item);
+                                itemsAdapter.notifyDataSetChanged();
                             }
                         })
-                        .setNegativeButton("Tidak", null)
-                        .setCancelable(false);
-                alert.show();
+                        .setNegativeButton("Ya", null)
+                        .show();
+                return true;
             }
         });
     }
+    public void initial() {
+        fab = findViewById(R.id.btn_fab);
+        lvItem = findViewById(R.id.Lv);
+
+    }
+
+    public void addtask() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Ingin Menambah Catatan ? ");
+        builder.setTitle("Add New");
+        final EditText inputField = new EditText(this);
+        builder.setView(inputField);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+
+                items.add(inputField.getText().toString());
+                itemsAdapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+
+        builder.create().show();
+    }
+
+
     private void ShowDialog() {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("Konfirmasi Keluar")
@@ -64,4 +116,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
 }
